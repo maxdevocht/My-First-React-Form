34dcb7a8e94df8c87921aa2aebe5b612bdd07da0
Max de Vocht <maxdevocht@hotmail.com>
2024-04-18 16:14:30 +0200

    Created Form

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import Form from './components/Form';
+import './index.css';
+import './App.css';
+
+function App() {
+
+  return (
+    <Form />
+  )
+}
+
+export default App
