f3803eb92d19f00c60033d7d34af02a25e32c003
Max de Vocht <maxdevocht@hotmail.com>
2024-04-19 11:38:06 +0200

    added more inputs and textarea

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,7 +3,7 @@ import React, { useState } from "react"
 export default function Form() {
 
   const [formData, useFormData] = useState(
-    {firstName: "", lastName: "", email: "", password: ""}
+    {firstName: "", lastName: "", mobile: "", number: "", email: "", comment: ""}
   )
 
   function handleChange(event) {
@@ -34,13 +34,13 @@ export default function Form() {
               <div className="flex flex-row gap-3 justify-between">
                 <div className="w-2/4">
                   <label htmlFor="firstName" className="block text-sm font-medium leading-6 text-gray-900">
-                    First Name
+                    Voornaam
                   </label>
                   <div className="mt-2">
                     <input
                       id="firstName"
                       name="firstName"
-                      type="firstName"
+                      type="text"
                       autoComplete="firstName"
                       required
                       className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-teal-600 sm:text-sm sm:leading-6"
@@ -52,13 +52,13 @@ export default function Form() {
 
                 <div className="w-2/4">
                   <label htmlFor="lastName" className="block text-sm font-medium leading-6 text-gray-900">
-                    Last Name
+                    Achternaam
                   </label>
                   <div className="mt-2">
                     <input
                       id="lastName"
                       name="lastName"
-                      type="lastName"
+                      type="text"
                       autoComplete="lastName"
                       required
                       className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-teal-600 sm:text-sm sm:leading-6"
@@ -69,9 +69,47 @@ export default function Form() {
                 </div>
               </div>
 
+              <div className="flex flex-row gap-3 justify-between">
+                <div className="w-2/4">
+                  <label htmlFor="mobile" className="block text-sm font-medium leading-6 text-gray-900">
+                    Telefoonnummer
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      id="mobile"
+                      name="mobile"
+                      type="text"
+                      autoComplete="mobile"
+                      required
+                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-teal-600 sm:text-sm sm:leading-6"
+                      onChange={handleChange}
+                      value={formData.mobile}
+                    />
+                  </div>
+                </div>
+
+                <div className="w-2/4">
+                  <label htmlFor="number" className="block text-sm font-medium leading-6 text-gray-900">
+                    Studentennummer
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      id="number"
+                      name="number"
+                      type="text"
+                      autoComplete="number"
+                      required
+                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-teal-600 sm:text-sm sm:leading-6"
+                      onChange={handleChange}
+                      value={formData.number}
+                    />
+                  </div>
+                </div>
+              </div>
+
               <div>
                 <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-                  Email address
+                  E-mailadres
                 </label>
                 <div className="mt-2">
                   <input
@@ -89,25 +127,18 @@ export default function Form() {
   
               <div>
                 <div className="flex items-center justify-between">
-                  <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
-                    Password
+                  <label htmlFor="comment" className="block text-sm font-medium leading-6 text-gray-900">
+                    Vraag of opmerking
                   </label>
-                  <div className="text-sm">
-                    <a href="#" className="font-semibold text-teal-600 hover:text-teal-500">
-                      Forgot password?
-                    </a>
-                  </div>
                 </div>
                 <div className="mt-2">
-                  <input
-                    id="password"
-                    name="password"
-                    type="password"
-                    autoComplete="current-password"
-                    required
+                  <textarea
+                    id="comment"
+                    name="comment"
+                    rows={3}
                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-teal-600 sm:text-sm sm:leading-6"
                     onChange={handleChange}
-                    value={formData.password}
+                    value={formData.comment}
                   />
                 </div>
               </div>
@@ -117,15 +148,15 @@ export default function Form() {
                   type="submit"
                   className="flex w-full justify-center rounded-md bg-teal-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-teal-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-teal-600"
                 >
-                  Sign in
+                  Verstuur
                 </button>
               </div>
             </form>
   
             <p className="mt-10 text-center text-sm text-gray-500">
-              Not a member?{' '}
+              Contact opnemen?{' '}
               <a href="#" className="font-semibold leading-6 text-teal-600 hover:text-teal-500">
-                Start a 14 day free trial
+                uitleen.nijmegen@ixperium.nl
               </a>
             </p>
           </div>
